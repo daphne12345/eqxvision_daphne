231f7d8acd9d5670e2971f94a605d89516a8646e
Daphne Theodorakopoulos <56087728+daphne12345@users.noreply.github.com>
2024-06-21 10:28:14 +0200

    Update __init__.py

--- eqxvision/models/classification/__init__.py
+++ eqxvision/models/classification/__init__.py
@@ -1,16 +1,3 @@
 from . import (
-    alexnet,
-    convnext,
-    densenet,
-    efficientnet,
-    googlenet,
-    mobilenetv2,
-    mobilenetv3,
-    regnet,
-    resnet,
-    shufflenetv2,
-    squeezenet,
-    swin,
-    vgg,
-    vit,
+    efficientnet
 )
